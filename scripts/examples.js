e0d92eecbdc582e87a3c8732dec3defc5724f144
Jefferson Daniel <jeffersondaniels@hotmail.com>
2016-07-02 13:43:04 -0300

    update site

--- scripts/examples.js
+++ scripts/examples.js
@@ -1,39 +1,9 @@
 (function(){
 
-  var exampleOne = new Map({
-     'name': 'example one'
-    ,'wrapperId':'exampleOne'
-    ,'zoom': 12
-    ,'mobile': false
-    ,'icon': 'images/library.svg'
-  });
-
-  var exampleTwo = new Map({
-     'name': 'exampleTwo'
-    ,'wrapperId':'exampleTwo'
-    ,'zoom': 16
-    ,'icon': 'images/bar.svg'
-    ,'mobile': false
-    ,'color': {
-       'street': '#14191f'
-      ,'roadText': '#bfcbda'
-      ,'importantPlaces': '#abb7c3'
-      ,'landscape': '#203246'
-      ,'landscape__man_made': '#203246'
-      ,'iconPlaces': [
-        { 'visibility': 'on' },
-        { 'invert_lightness': true },
-        { 'saturation': 41 },
-        { 'gamma': 1.71 },
-        { 'hue': '#005eff' }
-      ]
-    }
-  });
-
-  var exampleTree = new Map({
+  var example = new Map({
      'name': 'exampleTree'
-    ,'wrapperId':'exampleTree'
-    ,'zoom': 10
+    ,'wrapperId':'example'
+    ,'zoom': 16
     ,'icon': 'images/university.svg'
     ,'mobile': false
     ,'color': {

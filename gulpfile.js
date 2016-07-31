62b6484b26f8a5c5fc56f6cb9410c15a82640299
Jefferson Daniel <jeffersondanielss@gmail.com>
2016-07-31 18:42:19 -0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     clean = require('gulp-clean'),
-    concat = require('gulp-concat')
-    uglify = require('gulp-uglify');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    stylus = require('gulp-stylus'),
+    watch = require('gulp-watch'),
+    jade = require('gulp-jade');
 
 gulp.task('clean', () => {
   return gulp.src('dist/')
@@ -10,16 +13,33 @@ gulp.task('clean', () => {
 });
 
 gulp.task('jshint', () => {
-  return gulp.src('scripts/**.*')
+  return gulp.src('app/scripts/**.*')
   .pipe( jshint() )
   .pipe( jshint.reporter('default') );
 });
 
-gulp.task('uglify', ['clean'], () => {
-  return gulp.src(['scripts/map.js', 'scripts/main.js'])
+gulp.task('uglify', () => {
+  return gulp.src(['app/scripts/map.js', 'app/scripts/main.js'])
   .pipe( uglify() )
   .pipe( concat('all.min.js') )
-  .pipe( gulp.dest('dist/') );
+  .pipe( gulp.dest('dist/scripts/') );
 });
 
-gulp.task('default', ['jshint', 'uglify']);
+gulp.task('stylus', () => {
+  return gulp.src('app/styles/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('jade', () => {
+  gulp.src('./app/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./'))
+});
+
+
+gulp.task('watch', () => {
+  gulp.watch('app/scripts/*.js', ['jshint', 'uglify']);
+  gulp.watch('app/styles/**/*.styl', ['stylus']);
+  gulp.watch('app/*.jade', ['jade']);
+});
